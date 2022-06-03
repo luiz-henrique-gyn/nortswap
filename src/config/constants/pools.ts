0bf6692a3f9781bb7f60a2248aa5d60df25beaa2
Eric Prates <ericrprates@gmail.com>
2022-06-02 22:16:39 -0300

    chore: Added new NT-BNB LPs pool

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -63,19 +63,19 @@ const pools: SerializedPoolConfig[] = [
     isFinished: false,
     isVault: true,
   },
-  // {
-  //   sousId: 247,
-  //   stakingToken: serializedTokens.ntbnb,
-  //   earningToken: serializedTokens.nt,
-  //   contractAddress: {
-  //     56: '0x1F8233Ea0a59f7EfF3304147F1d96A7d73AFC7E9',
-  //   },
-  //   poolCategory: PoolCategory.CORE,
-  //   harvest: true,
-  //   tokenPerBlock: '0.06',
-  //   sortOrder: 1,
-  //   isFinished: false,
-  // },
+  {
+    sousId: 247,
+    stakingToken: serializedTokens['cake-lp'],
+    earningToken: serializedTokens.nt,
+    contractAddress: {
+      56: '0x311251741Ae63a8Fda31AD968e0577BDAc64394A',
+    },
+    poolCategory: PoolCategory.CORE,
+    harvest: true,
+    tokenPerBlock: '0.02',
+    sortOrder: 1,
+    isFinished: false,
+  },
 ]
 
 export default pools
